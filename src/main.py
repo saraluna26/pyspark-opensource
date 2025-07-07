90ef293d0cc2918080acc9373094c09da0e12558
saraluna26 <79517333+saraluna26@users.noreply.github.com>
2025-07-07 16:38:11 +0100

    Read first static file

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from pyspark.sql install SparkSession
+from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("OpenSource-Project").getOrCreate()
-df = spark.createDataFrame[(1, "A"), (2, "B"), (3, "C")] , ["id", "valor"]
-df.show()
+spark = SparkSession.builder.appName("MiProyecto").getOrCreate()
+# df = spark.createDataFrame([(1, "A"), (2, "B")], ["id", "valor"])
+# df.show()
 
 
 #read data from open source 
 #url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 
 #read data from static 
-
-df_ny = spark.read()
+df_ny = spark.read.option("header", "True").format("csv").load("/Users/sarasolis/pyspark-opensource/data/static/AB_NYC_2019.csv")
+df_ny.show()
