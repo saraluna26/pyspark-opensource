d4f9b5caeae82babf1af042aad471405f61670e7
saraluna26 <79517333+saraluna26@users.noreply.github.com>
2025-08-07 17:51:50 +0100

    Added handle nulls

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from utils.Utils import Utils
-from pyspark.sql.functions import desc, col, lit, concat
+from processing.Processing import Processing
+from pyspark.sql.functions import desc, col, lit, concat, when, struct
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 def load_df(spark) -> DataFrame:
     # df = spark.createDataFrame([(1, "A"), (2, "B")], ["id", "valor"])
@@ -22,76 +24,93 @@ def load_df(spark) -> DataFrame:
     print(Utils.showSchema(df_ny_method))
     return df_ny_method
 
-def stadistics_calls(df_ny_method:DataFrame) -> DataFrame:
-      Utils.stadistic_summary(df_ny_method).show()
+def stadistics_methods(df_ny_method:DataFrame) -> DataFrame:
+    Utils.stadistic_summary(df_ny_method).show()
 
-    Utils.sum(df_ny_method).show()
-    Utils.mean(df_ny_method).show()
+    # Utils.sum(df_ny_method).show()
+    # Utils.mean(df_ny_method).show()
 
-    Utils.max_values(df_ny_method).show()
+    # Utils.max_values(df_ny_method).show()
 
-    df_ny_method.orderBy(desc("price")).show(10)
+    # df_ny_method.orderBy(desc("price")).show(10)
 
-    df_new_column = Utils.new_column_price_category(df_ny_method)
-    df_new_column.select("name", "price", "Price Category").show()
+def create_category(df_ny_method:DataFrame) -> DataFrame:
+    df_new_column = Processing.new_column_price_category(df_ny_method)
+    # df_new_column.select("name", "price", "Price Category").show()
 
-    df_window = Utils.window_price_category(df_new_column)
-    df_window.select("name", "price", "Price Category", "total_num_reviews").show()
+    # df_window = Processing.window_price_category(df_new_column)
+    # df_window.select("name", "price", "Price Category", "total_num_reviews").show()
 
-    # Using grouo by - we lose the extra information as name or price
-    df_group_by = Utils.group_by_category(df_new_column)
-    df_group_by.select("Price Category", "total_agg_revies").show()
+    # # Using grouo by - we lose the extra information as name or price
+    # df_group_by = Processing.group_by_category(df_new_column)
+    # df_group_by.select("Price Category", "total_agg_revies").show()
 
-    
+    return df_new_column
+
+
 def json_test():
     static_path_json = "/Users/sarasolis/pyspark-opensource/data/static/data_json.json"
+    #wny the file below is not working fine?
     static_path_json2 = "/Users/sarasolis/pyspark-opensource/data/static/data_json_structure2.json"
+
+    schema = StructType([
+        StructField("policy_id", StringType(), True),
+        StructField("customer", StructType([
+            StructField("id", StringType(), True),
+            StructField("name", StringType(), True)
+        ]), True),
+        StructField("premium", StringType(), True),
+        StructField("issue_date", StringType(), True),
+        StructField("status", StringType(), True)
+    ])
+
+
     df_json = Utils.loadDataFrame(spark, static_path_json, True, "json")  #spark.read.format("json").load(static_path_json)
-    df_json_structure2 = spark.read.format("json").option("multiline", "true").load(static_path_json) 
-    
-    # print (df_json_structure2)
-    # df_json_structure2.show()
+    #df_json_structure2 = spark.read.format("json").option("multiline", "true").load(static_path_json) 
 
-    # df = spark.read.option("multiline", "true").json(static_path_json2)
-    # df.show()
+    df = spark.read.schema(schema).json(static_path_json)
 
-    # print(df_json)
-    # df_json.show()
+    #Testing data_example
+    # data_example = [
+    # {
+    #   "policy_id": "P001",
+    #   "customer": {
+    #     "id": "C123",
+    #     "name": None
+    #   },
+    #   "premium": "1000",
+    #   "issue_date": "2023-01-15",
+    #   "status": "in progress"
+    # },
+    # {
+    #   "policy_id": "P002",
+    #   "customer": None,
+    #   "premium": "not available",
+    #   "issue_date": "invalid_date",
+    #   "status": "cancelled"
+    # }
+    # ]
+    #df = spark.read.schema(schema).json(data_example)
+    #df = spark.createDataFrame(data_example)
 
-    data_example = [
-    {
-      "policy_id": "P001",
-      "customer": {
-        "id": "C123",
-        "name": "Sara Lee"
-      },
-      "premium": "1000",
-      "issue_date": "2023-01-15",
-      "status": "in progress"
-    },
-    {
-      "policy_id": "P002",
-      "customer": None,
-      "premium": "not available",
-      "issue_date": "invalid_date",
-      "status": "cancelled"
-    }
-    ]
-
-    df = spark.createDataFrame(data_example)
+  
     df.show()
-    df.select("customer.name", "customer.id").show()
-    df_customer_valuese = df.withColumn("customer_name", col("customer.name")).withColumn("customer_id", col("customer.id"))
 
+    #checking schema
+    # print(Utils.showSchema(df_customer_valuese))
+    # df_customer_valuese.printSchema()
     
-    print(Utils.showSchema(df_customer_valuese))
-    df_customer_valuese.printSchema()
-    df.withColumn("customer_name", lit(" name ")).show()
 
-
-    df.withColumn("customer_name", concat(col("customer.name"), lit(" name "))).show()
-    df.withColumn("policy_id", concat(col("policy_id"), lit (" PI"))).show()
-
+    #Testing concat() and lit()
+    # df.withColumn("customer_name", concat(col("customer.name"), lit(" name "))).show()
+    # df.withColumn("policy_id", concat(col("policy_id"), lit (" PI"))).show()
+    #df.withColumn("customer_name", lit(" name ")).show()
+    
+    print("Null control in struct columns ---------------------------------------------")
+    df = Processing.handle_struct_nulls(df)
+    df.select("policy_id", "customer", "customer_name", "customer_id").show()
+  
+     
 
 #main() -- execute always the content when import it
 
@@ -100,10 +119,43 @@ def json_test():
 if __name__ == "__main__":
   # print(dir(main))
   spark = SparkSession.builder.appName("MiProyecto").getOrCreate()
-  df_ny_method = load(spark)
+  df_ny_method = load_df(spark)
   #Calls for stadistics methods
-  stadistics_calls(df_ny_method)
+  #stadistics_methods(df_ny_method)
+  df_category = create_category(df_ny_method)
+  print("DF WORKING WITH ---------------------------------------------")
+  df_category.show()
 
-
-  #Some example testing with JSON files
+  #Some example testing with JSON files --------------------------------------------- 
   json_test()
+
+
+  print("Before cleaning null ---------------------------------------------")
+  df_nulls_counts = Processing.count_null(df_category)
+  #df_nulls_counts.show() # -- here we can see the nulls per column in a df.
+  print("Total nulls before remove: " +str(sum(df_nulls_counts.collect()[0].asDict().values())))
+
+  list_cols_with_nulls = []
+  dict_cols_with_nulls = df_nulls_counts.collect()[0].asDict()
+
+  for c, val in dict_cols_with_nulls.items():
+    if val != 0:
+      list_cols_with_nulls.append(c)
+    
+  print("List col with nulls: ")
+  print(list_cols_with_nulls)
+
+
+  print("After cleaning null:")
+  df_no_nulls = Processing.remove_null(df_category)
+
+  for col_with_null in list_cols_with_nulls: 
+    print(f"  - Check null after cleaning df for col {col_with_null}: " + str(df_no_nulls.filter(col(col_with_null).isNull()).count()))
+   
+  print("After cleaning null ---------------------------------------------")
+  df_nulls_counts = Processing.count_null(df_no_nulls)
+  # df_nulls_counts.show()
+  print("Total nulls after remove: " +str(sum(df_nulls_counts.collect()[0].asDict().values())))
+
+
+  Processing.filter_by_neighbourhood(df_category, "Manhattan").show()
--- src/processing/processing.py
+++ src/processing/processing.py
@@ -1,6 +1,81 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import when, col, desc, sum, to_timestamp, lit, struct
+from pyspark.sql.window import Window
+
 class Processing:
 
+    def new_column_price_category (df:DataFrame) -> DataFrame:
+        return df.withColumn("Price Category", when(col("price") <= 120, "Cheap").when(col("price") <= 500, "Medium").when(col("price") > 500, "Expensive").otherwise("No"))
+
+    def window_price_category(df:DataFrame) -> DataFrame:
+        window=Window.partitionBy("Price Category").orderBy(desc("price"))
+        return df.withColumn("total_num_reviews", sum("number_of_reviews").over(window))
+        #.withColumn("rank", rank().over(window))
+        #.withColumn("price_quintile"), nile(5).over(window)
+
+    def group_by_category(df:DataFrame) -> DataFrame:
+        return df.groupBy("Price Category").agg(sum("number_of_reviews").alias("total_agg_revies"))
+
+
+    def remove_null(df:DataFrame)-> DataFrame:
+        #example returns tuple df.dtype-> [('name', 'string'), ('age', 'int'), ('salary', 'double')]
+        string_cols = [c for c, t in df.dtypes if t == 'string']
+        numeric_cols = [c for c, t in df.dtypes if t in ['int', 'float', 'double']]
+        dates_cols = [c for c, t in df.dtypes if t == 'date']
+
+        # print("numeric cols ------------ " + str(numeric_cols))
+        # print("string cols ------------ " + str(string_cols))
+        # print("dates_cols cols ------------ " + str(dates_cols))
+
+        # para las fechas mejor usar when y otherewise porque na.fill a veces no funciona bien con las fechas porque no hace cast
+        # DateType, TimestampType, ArrayType, MapType, StructType, BinaryType, DecimalType, NullType -> does not work.
+        for c in dates_cols:
+            df = df.withColumn(c, when(col(c).isNull(), to_timestamp(lit('1970-01-01 00:00:00'))).otherwise(col(c)))
+
+        df_no_nulls = df.na.fill("NA", subset=string_cols) \
+             .na.fill(0, subset=numeric_cols)
+        return df_no_nulls
+
+    def count_null(df:DataFrame)-> DataFrame:
+        df_nulls_counts = df.select(
+            [sum(when(col(c).isNull(), 1).otherwise(0)).alias(c)
+        for c in df.columns
+        ])
+        return df_nulls_counts
+
+    def count_null_col(df:DataFrame, col_name) -> DataFrame:
+        return df.filter(col(col_name).isNull()).count()
+
+    def handle_struct_nulls(df:DataFrame) -> DataFrame:
+        #Data quality - handle nulls
+        default_struct = struct(
+            lit("NOIDMAP").alias("id"),
+            lit("NONAMEMAP").alias("name")
+        )
+
+        return df.withColumn(
+        'customer',
+        when(col('customer').isNull(), default_struct).otherwise(col('customer'))
+        ).withColumn(
+        'customer',
+        struct (
+            when(col('customer.name').isNull(), lit("noName")).otherwise(col('customer.name')).alias("name"),
+            when(col('customer.id').isNull(), lit("noID")).otherwise(col('customer.id')).alias("id")
+        )
+        ).withColumn('customer_name', col('customer.name')).withColumn('customer_id', col('customer.id'))
+
+    def filter_by_neighbourhood(df:DataFrame, neighbourhood) -> DataFrame:
+        return df.filter(col("neighbourhood_group") == neighbourhood)
+
+    def count_rows_by_room_type(df:DataFrame, roomtype) -> DataFrame:
+        pass
+
+
     
+
+    
+
+
 
 # Nivel 2: Filtrado y selección
 # Filtra las propiedades que están en el barrio de 'Manhattan'.
@@ -12,7 +87,6 @@ class Processing:
 # Filtra propiedades con precio mayor a 200 dólares.
 
 # Encuentra el precio máximo y mínimo de las propiedades en Brooklyn.
-
 
 
 
@@ -40,10 +114,6 @@ class Processing:
 # Extrae el año y mes de una columna de fecha (si existe alguna, por ejemplo, last_review).
 
 # Filtra propiedades que no tengan reviews (number_of_reviews = 0).
-
-
-
-
 
 
 
